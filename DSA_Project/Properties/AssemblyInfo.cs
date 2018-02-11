1b5ed37b728a09de5129ae7af47fe31546eda0a7
Neburel <christophspiering@hotmail.de>
2018-02-11 11:56:49 +0100

    Einstellungen AssemblyInfos

--- DSA_Project/Properties/AssemblyInfo.cs
+++ DSA_Project/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,7 +7,7 @@ using System.Runtime.InteropServices;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("DSA_Project")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Programm zur einfachen Verwaltung von DSA Briefen")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DSA_Project")]
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("de-DE")]
+
